e5b4b0419fa01978eb6a85a36c295ebef9288efa
Александар Ноневски <Aleksandar.Nonevski@students.finki.ukim.mk>
2023-08-30 18:48:21 +0200

    initial commit

---
+++ Milioner/Program.cs
@@ -0,0 +1,26 @@
+﻿using Milioner.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milioner
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Console.Write("Started");
+
+            var a = new QuizService();
+            Application.Run(new Form1());
+        }
+    }
+}
